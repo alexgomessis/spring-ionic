8753076e3db2ad39eb7f38af80b6ea6ed0ce7826
Alex Gomes <alex.gomes.sis@gmail.com>
2018-12-19 23:11:08 -0200

    Authentication

--- src/main/java/com/alexgomes/cursomc/services/ClienteService.java
+++ src/main/java/com/alexgomes/cursomc/services/ClienteService.java
@@ -8,17 +8,21 @@ import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.alexgomes.cursomc.domain.Cidade;
 import com.alexgomes.cursomc.domain.Cliente;
 import com.alexgomes.cursomc.domain.Endereco;
+import com.alexgomes.cursomc.domain.enums.Perfil;
 import com.alexgomes.cursomc.domain.enums.TipoCliente;
 import com.alexgomes.cursomc.dto.ClienteDTO;
 import com.alexgomes.cursomc.dto.ClienteNewDTO;
 import com.alexgomes.cursomc.repositories.ClienteRepository;
 import com.alexgomes.cursomc.repositories.EnderecoRepository;
+import com.alexgomes.cursomc.security.UserSS;
+import com.alexgomes.cursomc.services.exceptions.AuthorizationException;
 import com.alexgomes.cursomc.services.exceptions.DataIntegrityException;
 import com.alexgomes.cursomc.services.exceptions.ObjectNotFoundException;
 
@@ -30,7 +34,15 @@ public class ClienteService {
 	@Autowired
 	private EnderecoRepository enderecoRepository;
 
+	@Autowired
+	private BCryptPasswordEncoder pe;
+
 	public Cliente find(Integer id) {
+		UserSS user = UserService.authenticated();
+		if (user == null || !user.hasRole(Perfil.ADMIN) && !id.equals(user.getId())) {
+			throw new AuthorizationException("Acesso negado");
+		}
+
 		Optional<Cliente> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
@@ -69,12 +81,12 @@ public class ClienteService {
 	}
 
 	public Cliente fromDTO(ClienteDTO objDto) {
-		return new Cliente(objDto.getId(), objDto.getNome(), objDto.getEmail(), null, null);
+		return new Cliente(objDto.getId(), objDto.getNome(), objDto.getEmail(), null, null, null);
 	}
 
 	public Cliente fromDTO(ClienteNewDTO objDto) {
 		Cliente cli = new Cliente(null, objDto.getNome(), objDto.getEmail(), objDto.getCpfOuCnpj(),
-				TipoCliente.toEnum(objDto.getTipo()));
+				TipoCliente.toEnum(objDto.getTipo()), pe.encode(objDto.getSenha()));
 		Cidade cid = new Cidade(objDto.getCidadeId(), null, null);
 		Endereco end = new Endereco(null, objDto.getLogradouro(), objDto.getNumero(), objDto.getComplemento(),
 				objDto.getBairro(), objDto.getCep(), cli, cid);
--- src/main/java/com/alexgomes/cursomc/services/PedidoService.java
+++ src/main/java/com/alexgomes/cursomc/services/PedidoService.java
@@ -4,9 +4,13 @@ import java.util.Date;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.alexgomes.cursomc.domain.Cliente;
 import com.alexgomes.cursomc.domain.ItemPedido;
 import com.alexgomes.cursomc.domain.PagamentoComBoleto;
 import com.alexgomes.cursomc.domain.Pedido;
@@ -14,6 +18,8 @@ import com.alexgomes.cursomc.domain.enums.EstadoPagamento;
 import com.alexgomes.cursomc.repositories.ItemPedidoRepository;
 import com.alexgomes.cursomc.repositories.PagamentoRepository;
 import com.alexgomes.cursomc.repositories.PedidoRepository;
+import com.alexgomes.cursomc.security.UserSS;
+import com.alexgomes.cursomc.services.exceptions.AuthorizationException;
 import com.alexgomes.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -69,6 +75,16 @@ public class PedidoService {
 		itemPedidoRepository.saveAll(obj.getItens());
 		emailService.sendOrderConfirmationHtmlEmail(obj);
 		return obj;
+	}
+
+	public Page<Pedido> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		UserSS user = UserService.authenticated();
+		if (user == null) {
+			throw new AuthorizationException("Acesso negado");
+		}
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		Cliente cliente = clienteService.find(user.getId());
+		return repo.findByCliente(cliente, pageRequest);
 	}
 
 }
